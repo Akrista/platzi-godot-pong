9181b91024c25181fdac635a14028041cf979e9b
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2022-10-14 00:11:09 -0400

    Init

---
+++ Level.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var PlayerScore = 0
+var OponentScore = 0
+
+func _ready():
+	_restart_game()
+
+func _process(delta):
+	$MarcadorPlayer.text = str(PlayerScore)
+	$MarcadorOponente.text = str(OponentScore)
+
+func _restart_game():
+	$Ball.is_moving = false
+	$Ball.direction = Vector2.ZERO
+	$Ball.position = Vector2(960,540)
+	#$Ball.reset_ball()
+	$RestartTimer.start()
+	
+
+func _on_ArcoPlayer_body_entered(body):
+	if body is Ball:
+		OponentScore += 1
+		_restart_game()
+
+func _on_ArcoOponente_body_entered(body):
+	if body is Ball:
+		PlayerScore += 1
+		_restart_game()
---
+++ Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var speed = 400
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	elif Input.is_action_pressed("ui_down"):
+		direction.y += 1
+		
+	move_and_slide(direction * speed)
